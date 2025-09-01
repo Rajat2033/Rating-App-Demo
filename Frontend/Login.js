bae8e5bd4ba10489df94c22adb028dae9239553f
Pranavkaware <140547918+Pranavkaware@users.noreply.github.com>
2025-09-01 14:40:06 +0530

    Login.js

---
+++ Frontend/Login.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import API from "../api";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const { data } = await API.post("/auth/login", { email, password });
+      localStorage.setItem("token", data.token);
+      if (data.role === "ADMIN") navigate("/dashboard");
+      else navigate("/stores");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
